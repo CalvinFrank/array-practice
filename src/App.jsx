63803827781e774a6383d6477ae0a24ddedc4684
Calvin Frank <CalvinAndersonFrank@gmail.com>
2023-01-05 14:41:42 -0500

    1.2.2, cleaned up header

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        <h1>Tables</h1>
-        <h1>addition</h1>
-        <h1>subtraction</h1>
-        <h1>multiplcation</h1>
-        <h1>division</h1>
+        <h1 className="header-h1">Tables</h1>
+        <h1 className="header-h1">addition</h1>
+        <h1 className="header-h1">subtraction</h1>
+        <h1 className="header-h1">multiplcation</h1>
+        <h1 className="header-h1">division</h1>
       </header>
     </div>
   )
