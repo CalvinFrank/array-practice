510c949c4ac1130d93ead55ac1ea87ec04ccfc9e
Calvin Frank <CalvinAndersonFrank@gmail.com>
2023-01-05 14:23:50 -0500

    1.2.0, header made and needs to be fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 function App() {
   return (
     <div className="App">
+      <header className="header">
+        <h1>Tables</h1>
+        <h1>addition</h1>
+        <h1>subtraction</h1>
+        <h1>multiplcation</h1>
+        <h1>division</h1>
+      </header>
     </div>
   )
 }
