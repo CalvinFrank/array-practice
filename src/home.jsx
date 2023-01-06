02b9075bb26ca821d1844bff0572a4939728cc09
Calvin Frank <CalvinAndersonFrank@gmail.com>
2023-01-05 20:32:33 -0500

    1.3.21, quick fix.

--- src/home.jsx
+++ src/home.jsx
@@ -1,7 +1,7 @@
 function Home() {
     return (
         <div className="homediv">
-            <p>put your suggestgons for tables below!</p> 
+            <p>put your suggestions for tables below!</p> 
             <div className="suggestions">
                 <input type="text" /> 
                 <button>Enter</button>
