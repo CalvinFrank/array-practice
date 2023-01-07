6a4804a5d52dd9e7b2a209763fda66e31810315b
Calvin Frank <CalvinAndersonFrank@gmail.com>
2023-01-06 20:54:35 -0500

    1.5.0,  addition calc works [last commit was 1.4.9]

--- src/charts/add.jsx
+++ src/charts/add.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 function Add() {
+    // const [num1, setNum1] = useState();
+  const [sum, setSum] = useState(0);
+
+
   return (
     <div className="add">
       <p>put in the two numbers you want to add</p>
-      <input type="text" />
+      <input type="text" id="num1"/>
       <p>+</p>
-      <input type="text" />
-      <button>Submit</button>
+      <input type="text" id="num2"/>
+      <button type="submit" onClick={() => setSum(Number(num1.value)+Number(num2.value))}>Submit</button>
+      <br />
+      {sum}
     </div>
   );
 }
