339113f182541242d836cea53c4d360607816082
Calvin Frank <CalvinAndersonFrank@gmail.com>
2023-01-15 16:35:55 -0500

    1.9.0, suggestions work

--- src/home.jsx
+++ src/home.jsx
@@ -1,16 +1,20 @@
+import { useState } from "react"
 function Home() {
-    const suggestions = []
-    function suggest(e) {
-        suggestions.push(suggestBox.value)
-        console.log(...suggestions)
+    const [suggestions, setSuggestions] = useState([])
+    
+    function suggest() {
+        setSuggestions([...suggestions, suggestBox.value])
     }
+    const suggestionElements = suggestions.map((suggestion) => {
+        return <p>{suggestion}</p>
+    })
     return (
         <div className="homediv">
             <p>Put your suggestions for calculators below!</p> 
             <div className="suggestions">
                 <input type="text" id="suggestBox"/> 
                 <button onClick={suggest}>Enter</button>
-                <p>{...suggestions}</p>
+                <div>{...suggestionElements}</div>
             </div>
         </div>
     )
